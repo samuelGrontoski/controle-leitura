bf6e8b3391ef5e7866be43dcd3a43edec3470275
Samuel Grontoski <contatosamuelgrontoski@gmail.com>
2025-02-05 18:40:24 -0300

    feat: add classes

---
+++ backend/src/main/java/com/example/backend/domain/leituras/Leitura.java
@@ -0,0 +1,30 @@
+package com.example.backend.domain.leituras;
+
+import com.example.backend.domain.livro.Livro;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.Date;
+import java.util.UUID;
+
+@Table(name = "leitura")
+@Entity
+@Setter
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Leitura {
+    @Id
+    @GeneratedValue
+    private UUID id;
+    private Date data_inicio;
+    private Date data_termino;
+    private int pagina;
+
+    @ManyToOne
+    @JoinColumn(name = "livro_id")
+    private Livro livro;
+}
---
+++ backend/src/main/java/com/example/backend/domain/livro/Livro.java
@@ -0,0 +1,30 @@
+package com.example.backend.domain.livro;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.UUID;
+
+@Table(name = "livro")
+@Entity
+@Setter
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Livro {
+    @Id
+    @GeneratedValue
+    private UUID id;
+    private String titulo;
+    private String autor;
+    private String genero;
+    private int num_paginas;
+    private int ano_publicacao;
+    private String capa_url;
+}

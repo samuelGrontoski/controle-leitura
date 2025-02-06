e5a15b8707e1d43448feba9a20cf0f24bf49f7ed
Samuel Grontoski <contatosamuelgrontoski@gmail.com>
2025-02-06 10:55:01 -0300

    feat: add repository

--- backend/src/main/java/com/example/backend/repository/LeituraRepository.java
+++ backend/src/main/java/com/example/backend/repository/LeituraRepository.java
@@ -3,7 +3,5 @@ package com.example.backend.repository;
 import com.example.backend.entity.Leitura;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.UUID;
-
-public interface LeituraRepository extends JpaRepository<Leitura, UUID> {
+public interface LeituraRepository extends JpaRepository<Leitura, Long> {
 }

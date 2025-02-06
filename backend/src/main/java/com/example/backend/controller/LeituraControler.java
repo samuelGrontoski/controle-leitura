852164b8be73e7a48ee5b4d8020ae45c455f72b6
Samuel Grontoski <contatosamuelgrontoski@gmail.com>
2025-02-06 10:55:33 -0300

    feat: add controlers

---
+++ backend/src/main/java/com/example/backend/controller/LeituraControler.java
@@ -0,0 +1,38 @@
+package com.example.backend.controller;
+
+import com.example.backend.dto.LeituraDTO;
+import com.example.backend.dto.LivroDTO;
+import com.example.backend.service.LeituraService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping(value = "/leitura")
+@CrossOrigin
+public class LeituraControler {
+
+    @Autowired
+    private LeituraService leituraService;
+
+    @GetMapping
+    public List<LeituraDTO> listarTodos() {
+        return leituraService.listarTodos();
+    }
+
+    @PostMapping
+    public void inserir(@RequestBody LeituraDTO leitura) {
+        leituraService.inserir(leitura);
+    }
+
+    @PutMapping
+    public LeituraDTO alterar(@RequestBody LeituraDTO leitura) {
+        return leituraService.alterar(leitura);
+    }
+
+    @DeleteMapping("/{id}")
+    public void excluir(@PathVariable("id") Long id) {
+        leituraService.excluir(id);
+    }
+}
---
+++ backend/src/main/java/com/example/backend/controller/LivroController.java
@@ -0,0 +1,37 @@
+package com.example.backend.controller;
+
+import com.example.backend.dto.LivroDTO;
+import com.example.backend.service.LivroService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping(value = "/livro")
+@CrossOrigin
+public class LivroController {
+
+    @Autowired
+    private LivroService livroService;
+
+    @GetMapping
+    public List<LivroDTO> listarTodos() {
+        return livroService.listarTodos();
+    }
+
+    @PostMapping
+    public void inserir(@RequestBody LivroDTO livro) {
+        livroService.inserir(livro);
+    }
+
+    @PutMapping
+    public LivroDTO alterar(@RequestBody LivroDTO livro) {
+        return livroService.alterar(livro);
+    }
+
+    @DeleteMapping("/{id}")
+    public void excluir(@PathVariable("id") Long id) {
+        livroService.excluir(id);
+    }
+}

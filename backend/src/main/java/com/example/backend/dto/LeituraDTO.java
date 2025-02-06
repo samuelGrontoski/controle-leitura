3506b276315bf5ba629d84d469ea704420500259
Samuel Grontoski <contatosamuelgrontoski@gmail.com>
2025-02-06 10:55:12 -0300

    feat: add dto

--- backend/src/main/java/com/example/backend/dto/LeituraDTO.java
+++ backend/src/main/java/com/example/backend/dto/LeituraDTO.java
@@ -1,4 +1,28 @@
 package com.example.backend.dto;
 
-public record LeituraDTO(Long data_inicio, Long data_termino, int pagina) {
+import com.example.backend.entity.Leitura;
+import com.example.backend.entity.Livro;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.springframework.beans.BeanUtils;
+
+import java.util.Date;
+
+@Setter
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+public class LeituraDTO {
+
+    private Long id;
+    private Date data_inicio;
+    private Date data_termino;
+    private int pagina;
+    private Livro livro;
+
+    public LeituraDTO(Leitura leitura) {
+        BeanUtils.copyProperties(leitura, this);
+    }
 }
--- backend/src/main/java/com/example/backend/dto/LivroDTO.java
+++ backend/src/main/java/com/example/backend/dto/LivroDTO.java
@@ -1,4 +1,27 @@
 package com.example.backend.dto;
 
-public record LivroDTO(String titulo, String autor, String genero, int num_paginas, int ano_publicacao, String capa_url) {
+import com.example.backend.entity.Livro;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.springframework.beans.BeanUtils;
+
+@Setter
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+public class LivroDTO {
+
+    private Long id;
+    private String titulo;
+    private String autor;
+    private String genero;
+    private int num_paginas;
+    private int ano_publicacao;
+    private String capa_url;
+
+    public LivroDTO(Livro livro) {
+        BeanUtils.copyProperties(livro, this);
+    }
 }
